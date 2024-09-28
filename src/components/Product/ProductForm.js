cc7ead34615d8caa5e775cbfa7614560ba255dc3
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-09-28 13:05:21 +0300

    feat: create product forme

---
+++ src/components/Product/ProductForm.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+
+const ProductForm = ({ onSubmit, product }) => {
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+
+    useEffect(() => {
+        if (product) {
+            setName(product.name);
+            setPrice(product.price);
+            setDescription(product.description);
+            setAvailableQuantity(product.availableQuantity);
+            setCategory(product.category);
+        }
+    }, [product]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSubmit({ name, price, description, availableQuantity, category });
+        setName('');
+        setPrice('');
+        setDescription('');
+        setAvailableQuantity('');
+        setCategory('');
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="mb-4">
+            <input
+                type="text"
+                placeholder="Product Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+                className="border p-2 mr-2"
+            />
+            <input
+                type="number"
+                placeholder="Price"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                required
+                className="border p-2 mr-2"
+            />
+            <input
+                type="text"
+                placeholder="Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                required
+                className="border p-2 mr-2"
+            />
+            <input
+                type="number"
+                placeholder="AvailableQuantity"
+                value={availableQuantity}
+                onChange={(e) => setAvailableQuantity(e.target.value)}
+                required
+                className="border p-2 mr-2"
+            />
+            <input
+                type="text"
+                placeholder="Category"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+                required
+                className="border p-2 mr-2"
+            />
+            <button type="submit" className="bg-blue-500 text-white p-2">
+                {product ? 'Update Product' : 'Create Product'}
+            </button>
+        </form>
+    );
+};
+
+export default ProductForm;

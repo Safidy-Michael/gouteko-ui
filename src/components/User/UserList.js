30208bc44def26b782676b09d57464f0b92d22f0
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-09-28 12:05:16 +0300

    feat: create css for user List

--- src/components/User/UserList.js
+++ src/components/User/UserList.js
@@ -2,28 +2,32 @@ import React from 'react';
 
 const UserList = ({ users, onEdit, onDelete, onShow }) => {
     return (
-        <ul>
+        <div className="d-flex justify-content-end" style={{ height: '100vh' }}>
+        <ul className="list-unstyled position-absolute top-0 end-0 mt-4 me-5 custom-margin">
             {users.map(user => (
-                <li key={user.id} className="mb-4">
-                    {user.firstName} {user.lastName} - {user.email} 
+                <li key={user.id} className="top0 mb-4">
+                        {user.firstName} {user.lastName} - {user.email}
+                         
                     <button
                         onClick={() => onEdit(user)}
-                        className="ml-2 bg-yellow-500 text-white p-1">
+                        className="ml-2 bg-yellow-500 text-B p-1 ">
                         Edit
                     </button>
                     <button
                         onClick={() => onShow(user.id)} 
-                        className="ml-2 bg-blue-500 text-white p-1">
+                        className="ml-2 bg-blue-500 text-B p-1">
                         Show
                     </button>
                     <button
                         onClick={() => onDelete(user.id)}
-                        className="ml-2 bg-red-500 text-white p-1">
+                        className="ml-2 bg-red-500 text-B p-1">
                         Delete
                     </button>
                 </li>
             ))}
         </ul>
+        </div>
+
     );
 };
 

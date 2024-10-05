baa83a562ad6ef06901b0488380e715f325ae2bc
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-10-05 15:13:17 +0300

    chore: debug something in the user detail about the reason for the image display

--- src/components/User/UserDetails.js
+++ src/components/User/UserDetails.js
@@ -10,6 +10,7 @@ const UserDetails = () => {
         const fetchUser = async () => {
             try {
                 const userData = await getUserById(id); 
+                console.log('User data:', userData); 
                 setUser(userData); 
             } catch (error) {
                 console.error('Error fetching user:', error);
@@ -24,16 +25,25 @@ const UserDetails = () => {
     }
 
     return (
-            <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-
-                
-                    <h1 >User Details</h1>
-                    <div></div><p>Name: {user.firstName} {user.lastName}</p>
-                    <p>Email: {user.email}</p>
-                    <p>Address: {user.address}</p>
-                    <p>Phone Number: {user.phoneNumber}</p>
-                
+        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+            <h1>User Details</h1>
+            {user.imageBase64 ? (
+               <img
+               src={user.imageBase64}
+               alt="User"
+               className="w-32 h-32 rounded-full"
+           />
+           
+            ) : (
+                <p>No image available</p>
+            )}
+            <div>
+                <p>Name: {user.firstName} {user.lastName}</p>
+                <p>Email: {user.email}</p>
+                <p>Address: {user.address}</p>
+                <p>Phone Number: {user.phoneNumber}</p> 
             </div>
+        </div>
     );
 };
 

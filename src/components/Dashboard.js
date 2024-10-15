daddcb97df1527ae23c2e47cd5f2d3c229a041cc
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-10-15 11:25:36 +0300

    feat: Create Dashboard Component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useNavigate} from 'react-router-dom'; 
+
+function WelcomeDashboard({ username }) {
+    const history = useNavigate();
+
+    const handleLogout = () => {
+        history('/');
+    };
+
+    return (
+        <div className="d-flex justify-content-center align-items-center vh-100">
+            <div className="border rounded-lg p-4" style={{width: '500px', height: '400px'}}>
+                <h2 className="mb-4 text-center">Welcome to Dashboard</h2>
+                <p className="mb-4 text-center">Hello, {username}!</p>
+                <p className="text-center">You are logged in successfully.</p>
+                <div className="text-center">
+                    <button type="button" className="btn btn-primary mt-3" onClick={handleLogout}>Logout</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default WelcomeDashboard;
